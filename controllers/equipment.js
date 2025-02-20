56576fe9c6e275b9fe2d2e98de59469d7757f4aa
kpzo <153002207+kpzo@users.noreply.github.com>
2025-02-20 00:06:26 -0500

    update equipment

--- controllers/equipment.js
+++ controllers/equipment.js
@@ -3,11 +3,20 @@ const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
 const getAllEquipment = async (req, res) => {
-    res.send('get all equipment')
+    // leave createdBy out of find() to allow all users to see all equipment all the time
+    const equipment = await Equipment.find({}).populate('createdBy', 'name')
+    res.status(StatusCodes.OK).json({ equipment, cout: equipment.length })
 }
 
 const getEquipment = async (req, res) => {
-    res.send('get single equipment')
+    // leave userId out of the variable declaration to allow all users to see each single equipment listing
+    const { params: { id:equipmentId } } = req
+
+    const equipment = await Equipment.findOne({ _id: equipmentId, })
+    if (!equipment) {
+        throw new NotFoundError(`No equipment with id : ${equipmentId}`)
+    }
+    res.status(StatusCodes.OK).json({ equipment })
 }
 
 const createEquipment = async (req, res) => {
@@ -19,12 +28,38 @@ const createEquipment = async (req, res) => {
 }
 
 const updateEquipment = async (req, res) => {
-    res.send('update equipment')
+    const {
+        body: { status },
+        user: { userId },
+        params: { id:equipmentId }
+    } = req
+
+    if (status === 'pending') {
+        throw new BadRequestError('Please locate equipment and update status')
+    }
+    const equipment = await Equipment.findOneAndUpdate(
+        { _id: equipmentId },
+        req.body,
+        { new: true, runValidators: true }
+    )
+    if (!equipment) {
+        throw new NotFoundError(`No equipment with id : ${equipmentId}`)
+    }
+
+    res.status(StatusCodes.OK).json({ equipment})
 }
 
 const deleteEquipment = async (req, res) => {
-    res.send('delete equipment')
+    const { params: { id: equipmentId } } = req
+
+    const equipment = await Equipment.findByIdAndRemove({ _id: equipmentId })
+
+    if (!equipment) {
+        throw new NotFoundError(`No equipment with id : ${equipmentId}`)}
+
+    res.status(StatusCodes.OK).send('Item deleted.')
 }
+
 
 module.exports = {
     getAllEquipment,
